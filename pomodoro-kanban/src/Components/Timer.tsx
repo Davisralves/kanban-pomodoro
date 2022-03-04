b9655567f5c90b3f5d49f43619be5aaf001b3173
Davisralves <davialves324@gmail.com>
2022-03-04 12:04:08 -0300

    iniciando logica com minutos e segundos

--- pomodoro-kanban/src/Components/Timer.tsx
+++ pomodoro-kanban/src/Components/Timer.tsx
@@ -1,16 +1,48 @@
 import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 
+const seconds: string[] = [];
+for(let index = 59; index >= 0; index -= 1) {
+  if(index >= 10) {
+    seconds.unshift(index.toString());
+  } else seconds.unshift('0' + index.toString())
+}
+const defaultMinutes = 25;
+const minutes = seconds.filter((number) => defaultMinutes >= parseInt(number));
+console.log(minutes, seconds);
+
+
 export default function Timer() {
-	const [counter, setCounter] = useState(10);
+	const [seconds, setSeconds] = useState(0);
+	const [minutes, setMinutes] = useState(0);
 	const [button, setButton] = useState(false);
+
+  const endTimer = (): number => {
+    setButton(false);
+    return 0;
+  }
 
 	const timer = () => {
 		let interval: NodeJS.Timer = null;
 		const oneSecond = 1000;
 		if (button) {
 			interval = setInterval(
-				() => setCounter((counter) => (counter > 0 ? counter - 1 : 0)),
+				() =>
+					setSeconds((seconds) => {
+						if (seconds > 0) {
+							return seconds - 1;
+						}
+						else {
+              setMinutes((minutes) => {
+                if(minutes > 0) {
+                  return minutes -1
+                } else {
+                  return endTimer();
+                };
+              })
+              return 60;
+            };
+					}),
 				oneSecond
 			);
 		}
@@ -20,18 +52,18 @@ export default function Timer() {
 	useEffect(timer, [button]);
 
 	return (
-		<section id='TimerSection'>
-      <div className="flexbox">
-			<h1 data-testid="Timer">{counter}</h1>
-      </div>
-          <Button
-            id="Start"
-            onClick={() => setButton(!button)}
-            variant="danger"
-            size="lg"
-          >
-            {button ? "Stop" : "Start"}
-          </Button>
+		<section id="TimerSection">
+			<div className="flexbox">
+				<h1 data-testid="Timer">{`${minutes}:${seconds}`}</h1>
+			</div>
+			<Button
+				id="Start"
+				onClick={() => setButton(!button)}
+				variant="danger"
+				size="lg"
+			>
+				{button ? "Stop" : "Start"}
+			</Button>
 		</section>
 	);
 }
